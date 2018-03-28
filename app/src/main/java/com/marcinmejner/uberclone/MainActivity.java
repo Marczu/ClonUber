9e3cdf98953c2ecd5cd855c3aea9d8ab6d01f06c
Marcin Mejner <marczu09@gmail.com>
2018-03-28 18:03:24 +0200

    Budowanie logowanie dla kierowcy i konsumenta

--- app/src/main/java/com/marcinmejner/uberclone/MainActivity.java
+++ app/src/main/java/com/marcinmejner/uberclone/MainActivity.java
@@ -1,13 +1,37 @@
 package com.marcinmejner.uberclone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @BindView(R.id.driver)
+    private Button driver;
+    @BindView(R.id.customer)
+    private Button customer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        driver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 }
